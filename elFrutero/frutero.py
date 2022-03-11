8f934baa397e35196f239cbbcabd94a4a02e1383
Sbastdia <sbastdia@myuax.com>
2022-03-11 11:39:18 +0100

    algunos pequeños cambios

--- elFrutero/frutero.py
+++ elFrutero/frutero.py
@@ -5,19 +5,9 @@ import os
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 filename = os.path.join(current_dir, 'datos.csv')
-print("Hola")
-#filename = 'datos.csv'
 raw_data = open(filename)
 data = np.loadtxt(raw_data, delimiter=";",skiprows=1)
 data=pnd.DataFrame({'Pesos':data})
 stats = jmp.JMPEstadisticas(data["Pesos"])
 stats.analisisCaracteristica()
 
-
-#import csv
-#filename = "datos.csv"
-#with open(filename) as f:
-    #header= next(f)
-
-    #for h in header:
-        #print (h, end='')
